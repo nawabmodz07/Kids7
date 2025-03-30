c5e2aba198cfa91071138055b4ec28d6edbfb704
nawabmodz07 <s.idarthkhatik0@gmail.com>
2025-03-30 18:09:36 +0530

    Update dark.py

--- dark.py
+++ dark.py
@@ -26,7 +26,7 @@ KEY_PRICES = {
     'week': 500  # 500 Rs per week
 }
 ADMIN_IDS = [5898718899]
-BOT_TOKEN = "6343477003:AAFw8TG0BTmHG-uTPzcZ3fkV121DSYm6ZUM"
+BOT_TOKEN = "6341994875:AAEwq9VJrFwmTgsoSnlbvczFQU4mPKU5u_Y"
 thread_count = 900
 packet_size = 9
 ADMIN_FILE = 'admin_data.json'
